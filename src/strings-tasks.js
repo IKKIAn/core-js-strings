056f704999dea42f45d41c9dba64511d6dd9e493
IKKIAn <andrvers@mail.ru>
2025-10-24 16:38:52 +0300

    feat: implement countVowels  function

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -304,8 +304,8 @@ function containsSubstring(str, substring) {
  *   countVowels('aEiOu') => 5
  *   countVowels('XYZ') => 1
  */
-function countVowels(/* str */) {
-  throw new Error('Not implemented');
+function countVowels(str) {
+  return str.split('').filter((ch) => 'aeiouyAEIOUY'.includes(ch)).length;
 }
 
 /**
